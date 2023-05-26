cbc41e73662f0d1e979e808bea4e848d3fda35c1
“satya” <satya.ardhi786@gmail.com>
2023-05-26 11:23:55 +0530

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package pack01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("-----*****Hello World and Welcome to Jenkins Training*****-----");
+	}
+}
