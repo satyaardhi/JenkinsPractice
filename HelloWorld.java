4837d3223e918fbc5ea1e46dbe69d2fa1d3e5289
satyaardhi <100302133+satyaardhi@users.noreply.github.com>
2023-05-26 14:26:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 package pack01;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		System.out.println("-----*****Hello World and Welcome to Jenkins Training*****-----");
+		System.out.println("-----***** Made Some Changes *****-----");
 	}
 }
