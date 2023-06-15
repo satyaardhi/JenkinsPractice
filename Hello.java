0cdff0506b9d92c8fbae526cb3f7c8a7a6125e3c
satyaardhi <100302133+satyaardhi@users.noreply.github.com>
2023-06-15 14:34:09 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package pack02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----*****Hello World and Welcome to Jenkins Training*****-----");
+	}
+
+}
