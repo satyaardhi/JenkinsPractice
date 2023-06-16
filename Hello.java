305d3216ac4817db38edcb827f49c895cc99360b
satyaardhi <100302133+satyaardhi@users.noreply.github.com>
2023-06-16 11:13:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("-----*****Hello World and Welcome to Jenkins Training*****-----");
-		System.out.println("Made Changes");
+		System.out.println("Made Some Changes");
 	}
 
 }
