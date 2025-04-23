8393a2805314c2f52d5ad2f6e0eddf9c60352d39
satyaardhi <100302133+satyaardhi@users.noreply.github.com>
2025-04-23 19:34:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package pack01;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("-----***** Trying Webhook *****-----");
+		System.out.println("-----***** Trying the Webhook *****-----");
 	}
 }
